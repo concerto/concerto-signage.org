a7e2bd96dc064ff640f14821752f549f7914cf1a
August <august@inniogroup.com>
2014-04-19 15:34:15 -0400

    Introducing scopes for page categories

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,11 @@
 class Page < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
-
   searchkick text_start: [:title, :summary]
-
   mount_uploader :header_image, ImageUploader
+  belongs_to :category
+  
+  scope :v1_help, -> {joins(:category).where(categories: { name: "v1 Help"})}
+  scope :help, -> {joins(:category).where(categories: { name: "Help"})}
+  scope :announcements, -> {joins(:category).where(categories: { name: "Announcements"})}
 end
