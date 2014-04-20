e0f1dc758c40579df8c211b0b1edb74c8172c5e6
August <august@inniogroup.com>
2014-04-20 00:06:53 -0400

    An approach to email yaml in #12 involving an env var for dokku

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,2 +1,10 @@
 require 'yaml'
-ConcertoSignage::Application.config.authorized_emails = YAML.load_file("./config/email.yml")
+#when using paas environments, emails list can be passed in as an ENV var
+if ENV["EMAILS"]
+  env_emails = ENV["EMAILS"].split(',')
+end
+
+#read local file with comma seperated emails
+file_emails = File.read("./config/local_auth_emails").split(",")
+emails = env_emails | file_emails #put the arrays together excluding duplicates
+ConcertoSignage::Application.config.authorized_emails = emails.to_yaml
