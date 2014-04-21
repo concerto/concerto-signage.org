6aa63c61797605b1edd3be837869b77a8051940f
Haris Khan <github@onekhan.com>
2014-04-20 20:34:59 -0400

    Add Code button to tinymce organize routes.rb better

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   resources :categories
+  resources :pages
 
   get 'pages/list_v1help'
   get 'pages/list_help'
   get 'pages/list_blog'
-  resources :pages
   root to: 'pages#show', id: 'frontpage'
 
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
-
   devise_scope :user do
   	get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
   	get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
   post '/tinymce_assets' => 'tinymce_assets#create'
+  match "pages/system/*" => "/system/"
 
 end
