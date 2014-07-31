cf32817eed2a7cf7d9d4bb96b0fad21e411a2370
Brian R Zaik <brian.r.zaik@gmail.com>
2014-07-30 23:17:55 -0700

    Updated gitignore file; built out JavaScript and page styles a bit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,24 @@
 //= require turbolinks
 //= require_tree .
 //= require tinymce-jquery
+
+
+$(document).ready(function() {
+	$("#nav-search").hide();
+
+  $("#nav-search-toggle a").on("click", function(e) {
+    e.preventDefault();
+
+    if ( $(this).parents("ul").hasClass("search-on") ) {
+      $(this).parents("ul").find("li").show();
+      $("#nav-search").hide();
+      $(this).parents("ul").removeClass("search-on").addClass("search-off");
+    } else {
+      $(this).parents("ul").removeClass("search-off").addClass("search-on").find("li").hide();
+
+      $(this).parents("li").show();
+
+      $("#nav-search").show().find("input").focus(); 
+    }
+  });
+});
